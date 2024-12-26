25bfb19eb102673a7c9b009ae06730ac40a18fed
xupengboo <417129883@qq.com>
2024-12-26 16:20:34 +0800

    fix：canal配置

--- 6.大数据相关/xupengboo-flink-demo/src/main/java/com/xupengboo/flink/App.java
+++ 6.大数据相关/xupengboo-flink-demo/src/main/java/com/xupengboo/flink/App.java
@@ -4,6 +4,7 @@ import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -11,16 +12,12 @@ public class App {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // 模拟输入数据源
-        DataStream<String> text = env.fromElements(
-                "hello flink",
-                "flink is powerful",
-                "hello world"
-        );
+        DataStream<String> text = env.fromElements("hello", "world", "flink", "hello", "flink");
 
         // 转换逻辑：分割单词并计数
         DataStream<Tuple2<String, Integer>> wordCounts = text
-                .flatMap((String line, org.apache.flink.util.Collector<Tuple2<String, Integer>> out) -> {
-                    for (String word : line.split(" ")) {
+                .flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
+                    for (String word : value.split(" ")) {
                         out.collect(new Tuple2<>(word, 1));
                     }
                 })
@@ -28,9 +25,10 @@ public class App {
                 .keyBy(0)
                 .sum(1);
 
+        // 输出结果到文件
+        wordCounts.writeAsText("hdfs://vm-01:9000/output.txt").setParallelism(1);
         // 打印结果
         wordCounts.print();
-
         // 执行任务
         env.execute("Flink Word Count");
     }
