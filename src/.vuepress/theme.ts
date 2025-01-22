1626998109795bdc7ad2f0e24638a62a4660f561
xupengboo <417129883@qq.com>
2025-01-22 16:11:00 +0800

    fix：依赖重构

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -2,7 +2,7 @@ import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
-const password = process.env.JAVA_HOME || "123456";
+const password = process.env.ENCRYPT_PASSWORD || "123456";
 
 export default hopeTheme({
   hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
