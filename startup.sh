7e42f46eaffba7effa472fdea7a83e35ad970417
xupengboo <417129883@qq.com>
2025-02-10 16:34:55 +0800

    fix：密码修改

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 输入密码
-read -sp "Enter the encryption password: " PASSWORD
-echo  # 换行（避免密码输入后和下一行提示混在一起）
+# read -sp "Enter the encryption password: " PASSWORD
+# echo  # 换行（避免密码输入后和下一行提示混在一起）
 
 # 设置镜像名称
 IMAGE_NAME="xupengboo:2.0.0"
@@ -22,7 +22,7 @@ docker rm -f ${CONTAINER_NAME} || true  # 忽略没有找到容器的错误
 
 # 步骤 3: 运行新的容器
 echo "Running new container ${CONTAINER_NAME} from image ${IMAGE_NAME}..."
-docker run -d -p 80:80 --name ${CONTAINER_NAME} -e ENCRYPT_PASSWORD="$PASSWORD" ${IMAGE_NAME}
+docker run -d -p 80:80 --name ${CONTAINER_NAME} -e ENCRYPT_PASSWORD="123456" ${IMAGE_NAME}
 
 # 步骤 4: 删除所有悬挂的镜像（没有标签的镜像）
 echo "Removing dangling images..."
